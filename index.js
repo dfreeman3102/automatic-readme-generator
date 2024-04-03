5e1aff62fc12b9ff528d4740866cac6f819e7c06
dfreeman3102 <davidfreeman3102@gmail.com>
2024-04-03 17:06:04 -0400

    added prompts to get information from the user

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-// TODO: Include packages needed for this application
+const fs = requrie("fs");
+const inquire = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+inquire
+    .prompt([
+        {
+            type:"input",
+            name:"title",
+            message:"What is the title of your project?"
+        },
+        {
+            type:"input",
+            name:"description",
+            message:"What is the description of your project?"
+        },
+        {
+            type:"input",
+            name:"install",
+            message:"How do you install your project?"
+        },
+        {
+            type:"input",
+            name:"usage",
+            message:"What is the usage information of your project?"
+        },
+        {
+            type:"input",
+            name:"contributing",
+            message:"What are your guidelines to contribute to your project?"
+        },
+        {
+            type:"input",
+            name:"test",
+            message:"What are the tests of your project?"
+        },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    ])
+    .then((data) => {
+        // TODO: Create a function to write README file
+        function writeToFile(fileName, data) {}
+
+    })
 
 // TODO: Create a function to initialize app
 function init() {}
